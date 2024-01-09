acdd82f652f102fa30e9f6850454021f04e30b2a
wawerutimothy <waweru_timothy@yahoo.com>
2024-01-09 16:41:15 +0300

    updated search results

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const apiKey = 'put your api key';
+const apiKey = 'Your api key';
 const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
 
 app.use(express.json());
@@ -48,5 +48,15 @@ app.get('/products/:productId/offers', async (req, res) => {
         res.json(error);
     }
 })
-
+// get search Results
+app.get('/search/:searchQuery', async (req, res) => {
+    const { searchQuery } = req.params;
+    try {
+        const response = await request(`${baseUrl}&url=https://amazon.com/s?k=${searchQuery}`);
+        res.json(JSON.parse(response));
+    
+    } catch (error) {
+        res.json(error);
+    }
+})
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
